3f1392f2f7b74ee722b5ad058b84d834351c67b2
cpreza24 <cpreza93@yahoo.com>
2023-10-10 21:22:34 -0700

    adds the current time to the clock element

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Digital Clock');
+let clock = document.querySelector('.clock');
+
+//Variables to get the current hours minutes and seconds
+let date = new Date();
+let currentHour = date.getHours();
+let currentMinute = date.getMinutes();
+let currentSeconds = date.getSeconds();
+
+console.log(currentHour + ' ' + currentMinute + ' ' + currentSeconds);
+
+//adding the current time to the clock element
+clock.innerHTML = currentHour + ':' + currentMinute + ':' +currentSeconds;
+
+
+
+
+
+
+
+
+
